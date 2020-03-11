bc581dc438b076e3b68dc522d7a4a5632bd5c8d5
justdheja <justdheja@gmail.com>
2020-03-11 14:00:06 +0700

    yow

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,28 @@
       Final EEC
     </h1>
 
+    <div class="block">
+        <b-radio v-model="timpenjawab"
+            name="name"
+            native-value="Flint">
+            Flint
+        </b-radio>
+        <b-radio v-model="timpenjawab"
+            name="name"
+            native-value="Silver">
+            Silver
+        </b-radio>
+    </div>
+
     <div class="buttons flex-center">
-      <btn-pertanyaan nomor=1 pertanyaan="hello"></btn-pertanyaan>
+      <btn-pertanyaan nomor=1 pertanyaan="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sed quo dicta odit. Ipsa, quisquam. Maiores, temporibus libero facere porro dolorum esse explicabo eum iusto amet quibusdam, facilis suscipit pariatur." waktumenjawab=90 :timPenjawab="timpenjawab"></btn-pertanyaan>
       <btn-pertanyaan nomor=2 pertanyaan="hallo"></btn-pertanyaan>
-      <btn-pertanyaan nomor=1 pertanyaan="hello"></btn-pertanyaan>
-      <btn-pertanyaan nomor=2 pertanyaan="hallo"></btn-pertanyaan>
-      <btn-pertanyaan nomor=1 pertanyaan="hello"></btn-pertanyaan>
-      <btn-pertanyaan nomor=2 pertanyaan="hallo"></btn-pertanyaan>
-      <btn-pertanyaan nomor=2 pertanyaan="hallo"></btn-pertanyaan>
-      <btn-pertanyaan nomor=2 pertanyaan="hallo"></btn-pertanyaan>
+      <btn-pertanyaan nomor=3 pertanyaan="hello"></btn-pertanyaan>
+      <btn-pertanyaan nomor=4 pertanyaan="hallo"></btn-pertanyaan>
+      <btn-pertanyaan nomor=5 pertanyaan="hello"></btn-pertanyaan>
+      <btn-pertanyaan nomor=6 pertanyaan="hallo"></btn-pertanyaan>
+      <btn-pertanyaan nomor=7 pertanyaan="hallo"></btn-pertanyaan>
+      <btn-pertanyaan nomor=8 pertanyaan="hallo"></btn-pertanyaan>
     </div>
   </div>
 </template>
@@ -25,6 +38,11 @@ export default {
   name: 'App',
   components: {
     BtnPertanyaan
+  },
+  data(){
+    return{
+      timpenjawab: ''
+    }
   }
 }
 </script>
---
+++ src/components/ButtonPertanyaan.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <button class="button is-primary is-medium"
+            @click="isCardModalActive = true">
+            {{ nomor }}
+        </button>
+
+        <b-modal :active.sync="isCardModalActive" :width="640" scroll="keep">
+            <div class="card">
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-48x48">
+                                <img src="../assets/logotc.png" alt="Image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">{{ timPenjawab }}</p>
+                            <p class="subtitle is-6">waktu tersisa <strong>60</strong>s</p>
+                        </div>
+                    </div>
+
+                    <div class="content">
+                        <p>
+                        {{ pertanyaan }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </b-modal>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nomor: Number,
+        pertanyaan: String,
+        kategori: String,
+        timPenjawab: String,
+        waktumenjawab: Number
+    },
+    data(){
+        return{
+            isCardModalActive : false,
+            waktumenjawab: 60
+        }
+    }
+}
+</script>
